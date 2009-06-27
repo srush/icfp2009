a6f65c0f856ff2826bb1b569181ba1d276c29232
srush <sasha.rush@gmail.com>
2009-06-27 16:30:14 -0700

    blah

--- src/Main.hs
+++ src/Main.hs
@@ -31,16 +31,26 @@ main = do
   mem <- memToList initState
               --mapM_ (putStrLn . show) $ zip mem [0..] 
               --mapM_ (putStrLn . show) $ zip ops [0..]
+  let blank = [(2, 0.000),(3,0.000)]
+
+  ostates <-  runRounds ops ([(16000, 2001)] :  ([[(2,1502), (3, 0)]] ++ replicate 5989 blank ++ [[(2,-1044), (3, -1.16)]] ++ replicate 3196 blank ++ [[(2, -328.26), (3, 0.56882)]] ++ repeat blank)) initState 50000
+
+  --ostates <-  runRounds ops ([(16000, 3001)] :  (replicate 2634 blank ++ [[(2,0.0), (3, 1172)]] ++ replicate 4726 blank ++ [[(2, 0.0), (3, -367.63)]] ++ repeat blank)) initState 50000
+  -- ostates <-  runRounds ops ([(16000, 3004)] :  (replicate 2573 blank ++ [[(2,-1204), (3, 0.0)]] ++ replicate 4690 blank ++ [[(2, 391), (3, 0.0)]] ++ repeat blank)) initState 50000 
+ -- ostates <-  runRounds ops ([(16000, 3003)] :  (replicate 3 blank ++ [[(2,-224), (3, 0.0)]] ++ replicate 3457 blank ++ [[(2, 0), (3, -4310)]] ++ repeat blank)) initState 75000 
+
+  --ostates <-  runRounds ops ([(16000, 3004)] :  (repeat blank ++ [[(2,0.0), (3, 2163)]] ++ replicate 434531 blank ++ [[(2, 0.0), (3, -1010)]] ++ repeat blank)) initState 50000
  
-  ostates <-  runRounds ops ([(16000, 3004)] : (repeat [(2, 0.000),(3,0.000)])) initState 2
+        --ostates <-  runRounds ops ([(16000, 3001)] : (repeat [(2, 0.000),(3,0.000)])) initState 100000
 
               --mem2 <- memToList ostate
               --print $ show mem2               
               --mapM_ showPorts ostates
-  mapM_ showPorts ostates
+  --  mapM_ showPorts ostates
   ports <- mapM getPorts ostates
   let points = map portsToPos ports
-  let rad = (head ports) ! 4
+  let them = map portsToThem ports
+  --  let rad = (head ports) ! 4
   --print $ show rad
   --print $ show $ map portsToPos ports
               
@@ -51,7 +61,7 @@ main = do
 
   SDL.glSwapBuffers
    
-  Draw.draw $ Draw.scale drawScale drawScale $ mconcat $ (map drawPoint points ++  [drawEarth, drawRad rad])
+  Draw.draw $ Draw.scale drawScale drawScale $ mconcat $ (map (drawPoint (255,0,0,255)) them ++ map (drawPoint (255,255,0,255) ) points   ++  [drawEarth])
       
   SDL.glSwapBuffers
   waitClicks
@@ -74,6 +84,6 @@ main = do
 
 drawEarth = Draw.color (0,0,255, 255) $ Draw.scale 6300000 6300000 Draw.circle
 
-drawPoint p = Draw.translate p $ Draw.scale 50000 50000 Draw.circle
+drawPoint col p = Draw.color col $ Draw.translate p $ Draw.scale 50000 50000 Draw.circle
 
 drawRad r = mappend (Draw.color (0,0,0, 0) $ Draw.scale (r-80000) (r-80000) Draw.circle) (Draw.color (255,0,0, 255) $ Draw.scale r r Draw.circle)
--- src/Tests.hs
+++ src/Tests.hs
@@ -18,15 +18,16 @@ testOutput = TestCase $ do
               mem <- memToList initState
               --mapM_ (putStrLn . show) $ zip mem [0..] 
               --mapM_ (putStrLn . show) $ zip ops [0..]
- 
-              ostates <-  runRounds ops ([(16000, 1001)] : (repeat [(2, 1.000),(3,1.000)])) initState 100
+              let blank = [(2, 0.000),(3,0.000)]
+              ostates <-  runRounds ops ([(16000, 1001)] : [(2,0.0), (3, 2061)] : (replicate 31315 blank ++ [[(2,0.0), (3, 2061)]] ++ repeat blank)) initState 100000
 
               --mem2 <- memToList ostate
               --print $ show mem2               
               --mapM_ showPorts ostates
               -- mapM_ showPorts ostates
               ports <- mapM getPorts ostates
-              print $ show $ map portsToPos ports  
+              print $ show $ map portsToPos ports
+              
               return () 
     where showPorts st = do
                           ls <- portToList st
@@ -35,3 +36,4 @@ testOutput = TestCase $ do
             portToMap st 
 
 runReal = TestList $ [testOutput]
+
